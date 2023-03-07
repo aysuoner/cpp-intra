78ac9e50efd55804df5392d19dfcbb972678b270
aysuoner <aysuoner5@gmail.com>
2023-03-07 23:35:14 +0300

    cpp01 ex04 is ready for intrapush

--- CPP-01/ex04/main.cpp
+++ CPP-01/ex04/main.cpp
@@ -6,59 +6,72 @@
 /*   By: aoner <aoner@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 16:34:19 by aoner             #+#    #+#             */
-/*   Updated: 2023/01/19 11:27:34 by aoner            ###   ########.fr       */
+/*   Updated: 2023/03/07 23:31:14 by aoner            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "replace.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-void	ReplaceStringInPlace(std::string& line, const std::string& search,
-								const std::string& replace)
+std::string ft_replace(std::string cont ,std::string find, std::string replace)
 {
-    size_t pos = 0;
-    while ((pos = line.find(search, pos)) != std::string::npos)
-	{
-         line.replace(pos, search.length(), replace);
-         pos += replace.length();
+	size_t	pos = cont.find(find);
+    while (pos != std::string::npos)
+    {
+        cont.erase(pos , find.length());
+        cont.insert(pos, replace);
+        pos = cont.find(find);
     }
+    return cont;
 }
 
-void	create_and_copy(replace *replace_o, std::string argv1) //bunu header kütüphanesinde nereye yazmalıyım?
+std::ifstream file_check(std::string fileName)
 {
-	std::string line;
-	std::ifstream in_file(argv1);
-	std::ofstream out_file(replace_o->getfilename());
-	if (in_file && out_file)
+	std::ifstream file(fileName);
+	if (!file.is_open())
 	{
-		while (getline(in_file, line))
-		{
-			ReplaceStringInPlace(line, replace_o->gets1(), replace_o->gets2());
-			out_file << line << "\n";
-		}
-		std::cout << "Copy Finished \n";
+		std::cout << "File cannot be opened" << std::endl;
+		exit (EXIT_FAILURE);
 	}
-	else
-	{
-		std::cerr << "Cannot read File \n";
-	}
-	in_file.close();
-	out_file.close();
+	return (file);
 }
-//replace yasaklı fonksiyonmuş KALDIR!!!!!!
-int	main(int argc, char **argv)
+
+std::ofstream file_create(std::string fileName)
+{
+	std::ofstream newFile(fileName + ".replace", std::ios::trunc);
+	if (!newFile.is_open())
+	{
+		std::cout << "File cannot be opened" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	return (newFile);
+}
+
+int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
-		std::cerr << "You must enter three arguments!" << std::endl;
-		return(EXIT_FAILURE);
+		std::cout << "Usage: ./program [fileName] [search] [replace]" << std::endl;
+		return 1;
+	}
+	if (strlen(argv[1]) && strlen(argv[2]) && strlen(argv[3]))
+	{
+		std::string fileName;
+		std::ifstream file;
+		std::string line;
+		std::ofstream newFile;
+	
+		fileName = argv[1];
+		file = file_check(fileName);
+        std::getline(file, line, '\0');
+		file.close();
+		line = ft_replace(line, argv[2], argv[3]);
+		newFile = file_create(fileName);
+		newFile << line;
+        newFile.close();
 	}
 	else
-	{
-		replace replace_o = replace(argv[1], argv[2], argv[3]);
-		replace_o.setfilename(argv[1]);
-		create_and_copy(&replace_o, argv[1]);
-		replace_o.sets1(argv[2]);
-		replace_o.sets2(argv[3]);
-		return 0;
-	}
+		std::cout << "Arguments cannot be empty" << std::endl;
+	return (0);
 }
